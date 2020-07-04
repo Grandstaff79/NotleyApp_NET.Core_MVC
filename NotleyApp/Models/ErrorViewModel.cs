b589529cb0838fa101223da3e4fd079c18b2ed9d
Grandstaff79 <48068304+Grandstaff79@users.noreply.github.com>
2020-07-04 16:08:50 -0400

    Add project files.

---
+++ NotleyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NotleyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
